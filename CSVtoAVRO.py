65c7a06732a65838a1f1d3275f34d37f65c9f65f
bhaskarbhuyan <bhaskarbhuyan0012@gmail.com>
2018-04-24 14:47:57 +0530

    Initial Commit

---
+++ CSVtoAVRO.py
@@ -0,0 +1,21 @@
+import avro.schema
+import csv
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+
+schema = avro.schema.parse(open("user.avsc", 'r').read())
+
+writer = DataFileWriter(open("resultFile.avro", "wb"), DatumWriter(), schema)
+
+with open ('example.csv', 'rb') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',')
+    for row in reader:
+        writer.append({"name": row['name'], "id": int(row["id"]), "domain": row['domain']})
+
+
+writer.close()
+reader = DataFileReader(open("users.avro", "rb"), DatumReader())
+for user in reader:
+    print(user)
+
+reader.close()
